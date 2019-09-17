0f6bcd49e9eea64e1da35f955493457cb9126a24
jeetgpatel45 <50221604+jeetgpatel45@users.noreply.github.com>
2019-09-17 19:44:09 -0400

    Create slideshow.js

---
+++ public/assets/js/slideshow.js
@@ -0,0 +1,15 @@
+    //--------------------------SLIDESHOW--------------------------//
+    var slideIndex = 0;
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("images");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "flex";
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }
+    showSlides();
