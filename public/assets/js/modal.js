e6b0ff88f93a55549be0ac38b66a5928e88e3df0
nicksessa <50333367+nicksessa@users.noreply.github.com>
2019-09-18 20:56:24 -0400

    wrapped these functions in a document.ready function

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,14 +1,12 @@
-$("#loginBtn").on("click", function(){
-  event.preventDefault();
-  $("#myModal").css('display', 'block');
+$(document).ready(function() {
+  $("#loginBtn").on("click", function(){
+    event.preventDefault();
+    $("#myModal").css('display', 'block');
+  });
+  $(".close").on("click", function(){
+    $("#myModal").css('display', 'none')
+  });
 });
-
-
-
-$(".close").on("click", function(){
-  $("#myModal").css('display', 'none')
-});
-
 
 
 
