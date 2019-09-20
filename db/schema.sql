b06a0e79106c68d592ccef9684737a4effe842a0
nicksessa <50333367+nicksessa@users.noreply.github.com>
2019-09-19 21:32:11 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,24 @@
 CREATE DATABASE piedPiper_db;
 USE piedPiper_db;
 
-CREATE TABLE sales
-(
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	user_ID varchar(255) not null,
-	user_name varchar(255) NOT NULL,
-	song_title varchar(255) not null,
-	artist_name varchar(255) not null,
-	album_name varchar(255) not null,
-	updated_on TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-    created_on TIMESTAMP NOT NULL
+
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR(50) NOT NULL,
+password VARCHAR(10) NOT NULL,
+created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+last_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE orders (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR(255) NOT NULL,
+song_title VARCHAR(255) NOT NULL,
+artist_name VARCHAR(255) NOT NULL,
+album_name VARCHAR(255) NOT NULL,
+link  VARCHAR(255) NOT NULL,
+purchase_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
+purchase_price DECIMAL(5,2),
+created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+last_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
